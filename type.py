eee4ffaa1626150fec193a1beffc69853ed7eb3f
David Williams <williamsda3@vcu.edu>
2023-06-09 16:51:42 -0400

    updated json file

--- type.py
+++ type.py
@@ -2,47 +2,47 @@ import random
 import time
 import json
 
+  # File path to store the database
+database_file = "leaderboard.json"
 
+# Load the database from the file if it exists, or initialize an empty database
+try:
+    with open(database_file, "r") as file:
+        database = json.load(file)
+except FileNotFoundError:
+    database = []
+
+def save_database():
+    # Save the database to the file
+    with open(database_file, "w") as file:
+        json.dump(database, file)
+        
+def create_record(record):
+    database.append(record)
+    save_database()
+
+def read_records(field=None, value=None):
+    if field and value:
+        return [record for record in database if record.get(field) == value]
+    else:
+        return database
+
+def update_record(record_id, updates):
+    for record in database:
+        if record['id'] == record_id:
+            record.update(updates)
+            save_database()
+            break
+
+def delete_record(record_id):
+    for record in database:
+        if record['id'] == record_id:
+            database.remove(record)
+            save_database()
+            break
 
 def game():
-     # File path to store the database
-    database_file = "leaderboard.json"
-
-    # Load the database from the file if it exists, or initialize an empty database
-    try:
-        with open(database_file, "r") as file:
-            database = json.load(file)
-    except FileNotFoundError:
-        database = []
-
-    def save_database():
-        # Save the database to the file
-        with open(database_file, "w") as file:
-            json.dump(database, file)
-            
-    def create_record(record):
-        database.append(record)
-        save_database()
-
-    def read_records(field=None, value=None):
-        if field and value:
-            return [record for record in database if record.get(field) == value]
-        else:
-            return database
-
-    def update_record(record_id, updates):
-        for record in database:
-            if record['id'] == record_id:
-                record.update(updates)
-                save_database()
-                break
-
-    def delete_record(record_id):
-        for record in database:
-            if record['id'] == record_id:
-                database.remove(record)
-                save_database()
-                break
+   
         
     # Game setup 
     name = input('Enter name \n>>')
@@ -68,17 +68,17 @@ def game():
 
     print("Game over!")
     print("Final score:", score)
-    create_record({'name': name, 'score': score } )
+    # create_record({'name': name, 'score': score } )
     x = read_records()
     for entry in x:
         print(entry)
         # y = json.loads(str(entry))
         # print(y['name'])
     print(read_records())
-
-
     
+
+print(read_records())
   
 
-game()
+# game()
 # 0add leaderboard
